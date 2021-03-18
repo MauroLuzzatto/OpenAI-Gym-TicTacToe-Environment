362e4533e2a3d06fbb8ef2781844a4bb926a3f49
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2021-03-18 21:36:28 +0100

    ficed untracked files

--- gym-TicTacToe/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id="TTT-v0",
-    entry_point="gym_TicTacToe.envs:tictactoeEnv",
-    kwargs={'small': -1, 'large' : 10},
-)
--- gym-TicTacToe/gym_TicTacToe/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id="TTT-v0",
-    entry_point="gym_TicTacToe.envs:tictactoeEnv",
-)
